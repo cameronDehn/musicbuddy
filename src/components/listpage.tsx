c132cb0f6a7aec212fcb7c0f0fc4bb4f30cdf8b2
[Cameron Dehn] <[camreedpeace@gmail.com]>
2023-02-21 10:12:57 -0800

    add tailwind

--- src/components/listpage.tsx
+++ src/components/listpage.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import avatarTest from '../assets/standinavatar.jpg';
 
 const ListPage = ({ data }) => {
   return (
-    <div>
-      <h1>List Page</h1>
-      <ul>
-        {/* {data.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))} */}
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold mb-4">List Page</h1>
+      <ul className="space-y-2">
+        <li className="flex items-center justify-between border-b-2 border-gray-300 py-2">
+          <div className="flex items-center">
+            <img src={avatarTest} alt="avatar" className="w-6 h-6 rounded-full mr-2" />
+            <div>
+              <p className="font-bold text-sm">Elvis Presley</p>
+              <p className="text-sm">Hold My Hand</p>
+            </div>
+          </div>
+          <button className="w-8 h-8 rounded-full bg-green-500"></button>
+        </li>
       </ul>
     </div>
   );
